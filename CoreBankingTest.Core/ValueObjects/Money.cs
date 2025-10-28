1f2017d4b64d18fd4657808da74faa355410bc36
Ik3nna <ndukwuikenna@yahoo.com>
2025-10-28 14:40:20 +0100

    Addition elements of DDD, regarding customers, accounts, and transactions

---
+++ CoreBankingTest.Core/ValueObjects/Money.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreBanking.Core.ValueObjects
+{
+    public record Money
+    {
+        public decimal Amount { get; }
+        public string Currency { get; }
+
+        public Money(decimal amount, string currency = "NGN")
+        {
+            if (amount < 0)
+                throw new ArgumentException("Money amount cannot be negative");
+            Amount = amount;
+            Currency = currency;
+        }
+
+        public static Money operator +(Money a, Money b)
+        {
+            if (a.Currency != b.Currency)
+                throw new InvalidOperationException("Cannot add different currencies");
+            return new Money(a.Amount + b.Amount, a.Currency);
+        }
+
+        public static Money operator -(Money a, Money b)
+        {
+            if (a.Currency != b.Currency)
+                throw new InvalidOperationException("Cannot subtract different currencies");
+            return new Money(a.Amount  - b.Amount, a.Currency);
+        }
+
+
+    }
+}
